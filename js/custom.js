59860659df1fc1c77bbd1ade43819d7bb90bfd86
yakumwamba <brianlemba2@gmail.com>
2022-04-12 02:46:42 +0200

    refactoring

--- js/custom.js
+++ js/custom.js
@@ -30,7 +30,7 @@ function newNFTRow(nftItem) {
   testing.appendChild(newNode);
 
   console.log("New node", newNode);
-  // console.log(node)
+
 }
 let offset = 0;
 let previousResponse = [];
@@ -39,7 +39,7 @@ if (offset == 0) {
   fetchNFTs(0);
 }
 console.log("Loaded this script great");
-//var myChart = echarts.init(document.getElementById('main'));
+
 function fetchNFTs(offset) {
   fetch("https://api.hypereveal.xyz/v1/collections?sort=domain_authority&limit=5&offset=" + offset)
     .then((response) => response.json())
@@ -55,7 +55,7 @@ document.getElementById("nft-list").appendChild(tableHead)
       });
     });
 }
-// add event listener to the button to call the function
+
 document.getElementById("fetch-data").addEventListener("click", function () {
   offset += 100;
   fetchNFTs(offset);
@@ -72,39 +72,4 @@ document
   });
 const testing = document.getElementById("nft-list");
 console.log("Loaded this script great");
-// create a node list of all the nft cards
 
-//
-//testing.appendChild(newElement);
-
-console.log(testing);
-// write a function that gets json data from a url  using fetch and returns it
-
-
-//`<div  class="collection__content-wrapper bg-color-secondary">
-
-{
-  /* <div class="collection__image">
-    <div class="collection__image-wrapper">
-        <img src="images/pic-1.png" class="image-100" alt="" srcset="">
-    </div>
-    <p class="font-weight-medium">Space Riders NFT</p>
-</div>
-<div class="collection__price">
-    <p>0.029</p>
-    <div class="collection__price-image">
-        <img src="images/menu.png" class="image-100" alt="" srcset="">
-    </div>
-</div>
-<div class="collection__price">
-    <p>0.083</p>
-    <div class="collection__price-image">
-        <img src="images/menu.png" class="image-100" alt="" srcset="">
-    </div>
-</div>
-<p>726</p>
-<div class="collection__content-image-wrapper">
-    <img src="images/image-3.png" alt="" srcset="">
-</div>
-</div>` */
-}
